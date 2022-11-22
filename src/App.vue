7842973ebbfb0c1c11e53778bcc8e36f2c3a4997
NeyrisBH <113575840+NeyrisBH@users.noreply.github.com>
2022-11-22 17:46:26 -0500

    inicial

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
-    
+    <div id="app">
+        <router-view>
+            <div id="nav">
+                <router-link to="/">Inicio</router-link>
+                <router-link to="/pacientes">Inicio</router-link>
+                <router-link to="/doctores">Inicio</router-link>
+            </div>
+        </router-view>
+    </div>
 </template>
 
 <script>
-    export default {
-        data(){
-            return {titulo: 'TuCita.com'}
-        }
+export default {
+    data() {
+        return { titulo: 'TuCita.com' }
     }
+}
 </script>
 
 <style scoped>
