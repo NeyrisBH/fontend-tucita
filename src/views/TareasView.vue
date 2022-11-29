4f86bc6907a64cb52cdc75816852f6bb952bc67a
NeyrisBH <113575840+NeyrisBH@users.noreply.github.com>
2022-11-28 22:20:31 -0500

    tareas drud

--- src/views/TareasView.vue
+++ src/views/TareasView.vue
@@ -2,7 +2,8 @@
     <section class="container-fluid">
         <div class="card-header-flex">
             <h3>Tareas</h3>
-            <button class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#registerModal">Insertar</button>
+            <button class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#tareaModal"
+                @click="abrirModalRegistrar()">Insertar</button>
         </div>
         <div class="car-body">
             <table class="table table-hover">
@@ -22,11 +23,10 @@
                         <td>
                             <div class="buttons-acciones">
                                 <button type="button" class="btn btn-warning" data-bs-toggle="modal"
-                                    data-bs-target="#editarModal">
+                                    data-bs-target="#tareaModal" @click="abrirModalEditar(tarea)">
                                     <img src="../assets/svg/edit.svg" alt="">
                                 </button>
-                                <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                                    data-bs-target="#eliminarModal">
+                                <button type="button" class="btn btn-danger" @click="eliminarConfTarea(tarea.id)">
                                     <img src="../assets/svg/delete.svg" alt="">
                                 </button>
                             </div>
@@ -36,20 +36,64 @@
             </table>
         </div>
     </section>
-    <CrearTareaModal/>
+
+
+    <!-- Modal - Register -->
+    <div class="modal fade" id="tareaModal" tabindex="-1" role="dialog" aria-labelledby="registerModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content container">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">{{ tituloModal }}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form class="row g-4 container-fr">
+                        <div class="col-md-12">
+                            <label for="id">Id</label>
+                            <input v-model="id" type="number" class="form-control" id="id" name="id">
+                        </div>
+                        <div class="col-md-12">
+                            <label for="descripcion">Descripción</label>
+                            <input v-model="descripcion" type="text" class="form-control" id="descripcion"
+                                name="descripcion">
+                        </div>
+                        <div class="col-md-12">
+                            <label for="estado">Estado</label>
+                            <input v-model="estado" type="text" class="form-control" id="estado" name="estado">
+                        </div>
+                    </form>
+                    <div class="modal-footer">
+                        <button @click="crearTarea" class="btn btn-primary" v-if="btnCrear">Guardar</button>
+                        <button @click="actualizarTarea(id)" class="btn btn-primary"
+                            v-if="btnActualizar">Actualizar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import CrearTareaModal from '../components/CrearTareaModal.vue';
-
 export default {
     data() {
         return {
             titulo: "TuCita.com",
-            tareas: []
+            tituloModal: '',
+            tareas: [],
+            btnCrear: false,
+            btnActualizar: false,
+            id: 0,
+            descripcion: '',
+            estado: ''
         };
     },
     methods: {
+        limpiarForm() {
+            this.id = null,
+                this.descripcion = '',
+                this.estado = ''
+        },
         async consultaTareas() {
             const opciones = {
                 method: "GET",
@@ -77,7 +121,7 @@ export default {
         },
         async crearTarea() {
             const opciones = {
-                method: "PUT",
+                method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                     "Cache-Control": "no-cache",
@@ -99,15 +143,88 @@ export default {
                     else {
                         const data = await response.json();
                         console.log(data);
-                        TareasView.consultaTareas();
+                        this.limpiarForm();
+                        this.consultaTareas();
                     }
                 });
+        },
+        async actualizarTarea(id) {
+            const opciones = {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Cache-Control": "no-cache",
+                    "Authorization": "Basic dXNlcjplYzNmNjIxZi03YWIxLTQxZDctODJmNi1iZjY4ZjZiMDVmZTE="
+                },
+                body: JSON.stringify({
+                    id: this.id,
+                    descripcion: this.descripcion,
+                    estado: this.estado
+                })
+            };
+            fetch(`http://localhost:8080/api/tareas/${id}`, opciones)
+                .then(async (response) => {
+                    if (!response.ok) {
+                        const error = new Error(response.statusText);
+                        error.json = response.json();
+                        throw error;
+                    }
+                    else {
+                        const data = await response.json();
+                        console.log(data);
+                        this.consultaTareas();
+                    }
+                });
+        },
+        async eliminarTarea(id) {
+            const opciones = {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Cache-Control": "no-cache",
+                    "Authorization": "Basic dXNlcjpkZTAzMDgyNy0yZTI5LTQxMGUtYTgxNi0zMzUzMjlhNWU5YzI="
+                }
+            };
+            fetch(`http://localhost:8080/api/tareas/${id}`, opciones)
+                .then(async (response) => {
+                    if (!response.ok) {
+                        const error = new Error(response.statusText);
+                        error.json = response.json();
+                        throw error;
+                    }
+                    else {
+                        const data = await response.json();
+                        console.log(data);
+                        this.consultaTareas();
+                    }
+                });
+        },
+        eliminarConfTarea(id) {
+            if (!confirm('¿Esta seguro de eliminar la tarea?')) return;
+
+            this.eliminarTarea(id);
+        },
+        abrirModalRegistrar() {
+            this.id = null,
+                this.descripcion = '',
+                this.estado = '',
+                this.tituloModal = "Registrar Tareas",
+                this.btnCrear = true;
+            this.btnActualizar = false;
+        },
+        abrirModalEditar(data) {
+            this.id = data.id,
+                this.descripcion = data.descripcion,
+                this.estado = data.estado,
+                this.tituloModal = "Editar Cita",
+                this.btnCrear = false;
+            this.btnActualizar = true;
         }
     },
     beforeMount() {
         this.consultaTareas();
     },
-    components: { CrearTareaModal }
+    components: {}
 }
 </script>
 
